b8095a78440abafa4bf21a75d255a22836175591
Brandon Nightingale <brandon@jbnight.com>
2020-05-07 18:31:52 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -3,33 +3,33 @@ def create_an_empty_array
 end
 
 def create_an_array
- pets = ["Gus", "Cozie", "Toby", "Gizmo"]
+ array = ["Gus", "Cozie", "Toby", "Gizmo"]
 end
 
 def add_element_to_end_of_array(array, element)
- pets.push("Midnight")
+ array.push("Midnight")
 end
 
 def add_element_to_start_of_array(array, element)
- pets.unshift("Midnight")
+ array.unshift("Midnight")
 end
 
 def remove_element_from_end_of_array(array)
- pets.pop("Midnight")
+ array.pop("Midnight")
 end
 
 def remove_element_from_start_of_array(array)
-  pets.shift("Midnight")
+  array.shift("Midnight")
 end
 
 def retrieve_element_from_index(array, index_number)
- pets[1]
+ array[1]
 end
 
 def retrieve_first_element_from_array(array)
- pets[0]
+ array[0]
 end
 
 def retrieve_last_element_from_array(array)
- pets[-1]
+ array[-1]
 end
